daefa42d250ba5f71c40217e2efb0cb1b45b93e0
Curvu <curvu6@gmail.com>
2023-01-31 09:37:12 +0000

    players and queue arrays

---
+++ src/objects/vars.js
@@ -0,0 +1,6 @@
+const players = [{"name":"Caps","team":"G2","wins":0,"losses":0,"games":0},{"name":"Curvu","team":"G2","wins":0,"losses":0,"games":0},{"name":"Jankos","team":"Heretics","wins":0,"losses":0,"games":0},{"name":"Yike","team":"G2","wins":0,"losses":0,"games":0},{"name":"Zeradyn","team":"AUR","wins":0,"losses":0,"games":0}];
+const queue = [];
+
+const arrays = { players, queue }
+
+module.exports = arrays;
