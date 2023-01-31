b1311d57c1f9de3cd5707fb4a36e52b9567d0da2
Curvu <curvu6@gmail.com>
2023-01-31 12:26:29 +0000

    now the player can be added to the queue

--- src/commands/Community/joinQueue.js
+++ src/commands/Community/joinQueue.js
@@ -1,12 +1,39 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const queue = require('../../objects/queue.js')
+const Database = require('../../objects/Database.js');
+const db = new Database();
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('queue')
     .setDescription('Join the queue for a game'),
     async execute(interaction, client) {
-        await interaction.reply('Pong!');
+        // get the guild display name of the user
+        const userId = interaction.user.id;
+        const username = interaction.guild.members.cache.get(userId).displayName;
+
+        const player = await db.getPlayer(username);
+        console.log(player);
+
+        if (player == null) {
+            await interaction.reply({ content: `Please register yourself with /register`, ephemeral: true });
+            return;
+        }
+
+        const queue = require('../../objects/queue.js');
+        // check if the player is already in the queue
+        queue.forEach((p) => {
+            if (p.name === player.name) {
+                interaction.reply({ content: `You are already in the queue`, ephemeral: true });
+                return;
+            }
+        });
+
+        // add the player in the end of the queue
+        queue.push(player);
+        const fs = require('fs');
+        fs.writeFileSync('./src/objects/queue.js', `module.exports = ${JSON.stringify(queue)}`);
+
+        await interaction.reply({ content: `You have joined the queue`, ephemeral: true });
     }
 }
