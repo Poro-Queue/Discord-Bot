8052fad4b6ec42763490df445e4dafbbe71f0b06
Curvu <curvu6@gmail.com>
2023-02-01 17:31:29 +0000

    testing subcommands

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -1,10 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+// This is the main command, call a subcommand with /ping <subcommand>
+
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('This is a ping command'),
+        .setName('ping')
+        .setDescription('This is a ping command')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('pong')
+                .setDescription('This is a pong command'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('foo')
+                .setDescription('This is a foo command')),
     async execute(interaction, client) {
-        await interaction.reply('Pong!');
+        if (interaction.options.getSubcommand() === 'pong') {
+            await interaction.reply('Pong!');
+        } else if (interaction.options.getSubcommand() === 'foo') {
+            await interaction.reply('Foo!');
+        }
     }
-}
+};
