13f89e0e49b46c7d933489c554c2b1cdc730c3a1
Curvu <curvu6@gmail.com>
2023-02-04 16:21:50 +0000

    better test

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -2,6 +2,31 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // This is the main command, call a subcommand with /ping <subcommand>
 
+const Game = require('../../objects/Game.js');
+const players = {
+    Top: [
+        {"name":"BrokenBlade","team":"G2","role":"Top","ign":"TopFather","wins":0,"losses":0,"games":0},
+        {"name":"Wunder","team":"FNC","role":"Top","ign":"Martin","wins":0,"losses":0,"games":0}
+    ],
+    Jungle: [
+        {"name":"Jankos","team":"TH","role":"Jungle","ign":"thePizzaBox","wins":0,"losses":0,"games":0},
+        {"name":"Yike","team":"G2","role":"Jungle","ign":"Yike","wins":0,"losses":0,"games":0}
+    ],
+    Mid: [
+        {"name":"Caps","team":"G2","role":"Mid","ign":"Rasmus","wins":0,"losses":0,"games":0},
+        {"name":"Perkz","team":"VIT","role":"Mid","ign":"Perkz","wins":0,"losses":0,"games":0}
+    ],
+    ADC: [
+        {"name":"Neon","team":"VIT","role":"ADC","ign":"Neon","wins":0,"losses":0,"games":0},
+        {"name":"Rekkles","team":"FNC","role":"ADC","ign":"Rekkles","wins":0,"losses":0,"games":0}
+    ],
+    Support: [
+        {"name":"Mikyx","team":"G2","role":"Support","ign":"Mikyx","wins":0,"losses":0,"games":0},
+        {"name":"Kaiser","team":"VIT","role":"Support","ign":"Kaiser","wins":0,"losses":0,"games":0}
+    ]
+}
+const game = new Game(1, players);
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
@@ -15,36 +40,13 @@ module.exports = {
                 .setName('foo')
                 .setDescription('This is a foo command')),
     async execute(interaction, client) {
+        const guild = client.guilds.cache.get(interaction.guildId)
         if (interaction.options.getSubcommand() === 'pong') {
-            const Game = require('../../objects/Game.js');
-            const players = {
-                Top: [
-                    {"name":"BrokenBlade","team":"G2","role":"Top","ign":"TopFather","wins":0,"losses":0,"games":0},
-                    {"name":"Wunder","team":"FNC","role":"Top","ign":"Martin","wins":0,"losses":0,"games":0}
-                ],
-                Jungle: [
-                    {"name":"Jankos","team":"TH","role":"Jungle","ign":"thePizzaBox","wins":0,"losses":0,"games":0},
-                    {"name":"Yike","team":"G2","role":"Jungle","ign":"Yike","wins":0,"losses":0,"games":0}
-                ],
-                Mid: [
-                    {"name":"Caps","team":"G2","role":"Mid","ign":"Rasmus","wins":0,"losses":0,"games":0},
-                    {"name":"Perkz","team":"VIT","role":"Mid","ign":"Perkz","wins":0,"losses":0,"games":0}
-                ],
-                ADC: [
-                    {"name":"Neon","team":"VIT","role":"ADC","ign":"Neon","wins":0,"losses":0,"games":0},
-                    {"name":"Rekkles","team":"FNC","role":"ADC","ign":"Rekkles","wins":0,"losses":0,"games":0}
-                ],
-                Support: [
-                    {"name":"Mikyx","team":"G2","role":"Support","ign":"Mikyx","wins":0,"losses":0,"games":0},
-                    {"name":"Kaiser","team":"VIT","role":"Support","ign":"Kaiser","wins":0,"losses":0,"games":0}
-                ]
-            }
-            const game = new Game(1, players);
-            const guild = client.guilds.cache.get(interaction.guildId)
             game.startGame(guild);
-            await interaction.reply('Pong!');
+            await interaction.reply({ content: 'Pong!', ephemeral: true});
         } else if (interaction.options.getSubcommand() === 'foo') {
-            await interaction.reply('Foo!');
+            game.endGame(guild);
+            await interaction.reply({ content: 'Foo!', ephemeral: true});
         }
     }
 };
