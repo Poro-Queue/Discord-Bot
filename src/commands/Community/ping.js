d7d847ab5f90b9403642ecebc8c9da47bd34ddff
Curvu <curvu6@gmail.com>
2023-02-05 11:07:28 +0000

    no more tests here

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -2,31 +2,6 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // This is the main command, call a subcommand with /ping <subcommand>
 
-const Game = require('../../objects/Game.js');
-const players = {
-    Top: [
-        {"name":"BrokenBlade","team":"G2","role":"Top","ign":"TopFather","wins":0,"losses":0,"games":0},
-        {"name":"Wunder","team":"FNC","role":"Top","ign":"Martin","wins":0,"losses":0,"games":0}
-    ],
-    Jungle: [
-        {"name":"Jankos","team":"TH","role":"Jungle","ign":"thePizzaBox","wins":0,"losses":0,"games":0},
-        {"name":"Yike","team":"G2","role":"Jungle","ign":"Yike","wins":0,"losses":0,"games":0}
-    ],
-    Mid: [
-        {"name":"Caps","team":"G2","role":"Mid","ign":"Rasmus","wins":0,"losses":0,"games":0},
-        {"name":"Perkz","team":"VIT","role":"Mid","ign":"Perkz","wins":0,"losses":0,"games":0}
-    ],
-    ADC: [
-        {"name":"Neon","team":"VIT","role":"ADC","ign":"Neon","wins":0,"losses":0,"games":0},
-        {"name":"Rekkles","team":"FNC","role":"ADC","ign":"Rekkles","wins":0,"losses":0,"games":0}
-    ],
-    Support: [
-        {"name":"Mikyx","team":"G2","role":"Support","ign":"Mikyx","wins":0,"losses":0,"games":0},
-        {"name":"Kaiser","team":"VIT","role":"Support","ign":"Kaiser","wins":0,"losses":0,"games":0}
-    ]
-}
-const game = new Game(1, players);
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
@@ -42,10 +17,8 @@ module.exports = {
     async execute(interaction, client) {
         const guild = client.guilds.cache.get(interaction.guildId)
         if (interaction.options.getSubcommand() === 'pong') {
-            game.startGame(guild);
             await interaction.reply({ content: 'Pong!', ephemeral: true});
         } else if (interaction.options.getSubcommand() === 'foo') {
-            game.endGame(guild);
             await interaction.reply({ content: 'Foo!', ephemeral: true});
         }
     }
