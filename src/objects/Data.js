a80f434f29abf1d7ed175de7b1b266b44bef80b0
Curvu <curvu6@gmail.com>
2023-03-01 17:57:30 +0000

    better addPlayer

--- src/objects/Data.js
+++ src/objects/Data.js
@@ -5,14 +5,22 @@ let players = [];
 let queue = [];
 let games = [];
 
+// TODO: get the players from the API instead of the database
+
 module.exports = {
     initialize: async () => players = await db.getPlayers(),
     getPlayers: () => players,
     getQueue: () => queue,
     getGames: () => games,
-    addPlayer: (player) => players.push(player),
+    addPlayer: (player) => {
+        // verify if the player.name is already in the database
+        if (players.find((p) => p.getName() === player.getName())) return false;
+        db.addPlayer(player);
+        players.push(player);
+        return true;
+    },
     addPlayerToQueue: (player) => queue.push(player),
     addGame: (game) => games.push(game),
     updateQueue: (newQueue) => queue = newQueue,
-    updatePlayers: () => this.initialize()
+    updatePlayers: () => initialize()
 }
